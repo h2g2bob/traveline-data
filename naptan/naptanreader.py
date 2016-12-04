f52444fb60c4f2bbe60d4621605bfe6d0967814a
David Batley <git@dbatley.com>
2016-12-04 22:43:58 +0000

    Print stuff to stderr, so stdout has only useful data

--- naptan/naptanreader.py
+++ naptan/naptanreader.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from lxml import etree
 import zipfile
+import logging
 
 # Appears to be:
 # <Place>
@@ -28,38 +29,46 @@ def cleanup(element):
 	while element.getprevious() is not None:
 		del element.getparent()[0]  # clean up preceding siblings
 
-def avg(values):
-	values = tuple(values)
-	return sum(values) / len(values)
-
 def xpath(elem, path):
 	return elem.xpath(path, namespaces={"naptan": "http://www.naptan.org.uk/"}, smart_strings=False)
 
-def maybe_content(elem, path):
+def float_content(elem, path):
+	values = xpath(elem, path)
+	if len(values) == 1:
+		return float(values[0])
+	elif len(values) == 0:
+		return None
+	else:
+		logging.info("float_content: %r", values)
+		return sum(float(x) for x in values) / len(values)
+
+def text_content(elem, path):
 	values = xpath(elem, path)
 	if len(values) == 1:
 		return values[0]
 	elif len(values) == 0:
 		return None
 	else:
-		raise ValueError(values)
+		logging.info("text_content: %r", values)
+		return "".join(path)
 
 def handle_stoppoint(elem):
 	try:
 		if elem.get("Status") != "active":
 			return
 
-		name = maybe_content(elem, ".//naptan:Descriptor/naptan:CommonName/text()")
-		code = maybe_content(elem, ".//naptan:NaptanCode/text()")
-		latitude = avg(float(x) for x in xpath(elem, ".//naptan:Latitude/text()"))
-		longitude = avg(float(x) for x in xpath(elem, ".//naptan:Longitude/text()"))
+		name = text_content(elem, ".//naptan:Descriptor/naptan:CommonName/text()")
+		code = text_content(elem, ".//naptan:NaptanCode/text()")
+		latitude = float_content(elem, ".//naptan:Latitude/text()")
+		longitude = float_content(elem, ".//naptan:Longitude/text()")
 
 		print(code, name, latitude, longitude)
 
 	except Exception:
-		print(etree.tostring(elem))
+		logging.info('problem string %r', etree.tostring(elem))
 		raise
 
 
 if __name__ == '__main__':
+	logging.basicConfig()
 	main()
