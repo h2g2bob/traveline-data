d494b634aa90f801056f458c7d766fffda62c92e
David Batley <git@dbatley.com>
2017-01-30 01:17:12 +0000

    let file output a digraph

--- create_file.py
+++ create_file.py
@@ -9,9 +9,19 @@ import json
 def format_json(points):
 	return json.dumps(points, indent=4)
 
+def format_dot(points):
+	return 'digraph {\n' + '\n'.join(
+		'"{}" -> "{}";'.format(point['from']['id'], point['to']['id'])
+		for point in points
+	) + '\n}'
+
 def main():
+	format_type='dot'
 	with psycopg2.connect("dbname=travelinedata") as conn:
-		format_function = format_json
+		format_function = {
+			'json': format_json,
+			'dot': format_dot
+		}[format_type]
 		print(format_function(line_segments_in_boundingbox(conn, 51.48, -2.52, 51.99, -2.40)))
 
 if __name__ == '__main__':
