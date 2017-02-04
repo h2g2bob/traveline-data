d22dc8dcbf463e7cea68c6963a556350a120a6b7
David Batley <git@dbatley.com>
2017-02-04 20:33:23 +0000

    make sure we report all the stops we need, even for lines which escape the bounding box

--- tlparser/query_boundingbox.py
+++ tlparser/query_boundingbox.py
@@ -69,7 +69,9 @@ def line_segments_and_stops_in_boundingbox(conn, minlat, minlong, maxlat, maxlon
 				timing.from_stoppoint,
 				timing.to_stoppoint,
 				sum(case when days_mask & %s != 0 then """ + hour_column + """ else 0 end) AS frequency,
-				array_agg(distinct line.line_name) AS line_names
+				array_agg(distinct line.line_name) AS line_names,
+				max(width(jp_bbox.bounding_box)) as box_width,
+				max(height(jp_bbox.bounding_box)) as box_height
 
 			FROM jptiminglink timing
 			JOIN journeypattern_service_section section USING (jpsection_id)
@@ -81,7 +83,9 @@ def line_segments_and_stops_in_boundingbox(conn, minlat, minlong, maxlat, maxlon
 			""", (day_of_week, minlat, minlong, maxlat, maxlong,))
 		bus_stop_pairs = []
 		atcocode_list = set()
-		for from_id, to_id, frequency, line_names in cur:
+		tollerance_height = 0.0
+		tollerance_width = 0.0
+		for from_id, to_id, frequency, line_names, box_width, box_height in cur:
 			bus_stop_pairs.append({
 				"from": from_id,
 				"to": to_id,
@@ -89,6 +93,14 @@ def line_segments_and_stops_in_boundingbox(conn, minlat, minlong, maxlat, maxlon
 				"line_names": line_names})
 			atcocode_list.add(from_id)
 			atcocode_list.add(to_id)
+
+			# expand the next query so we can draw lines which have
+			# one stop inside and one outside the bounding box
+			# (there must be a better way to do this?)
+			if box_width > tollerance_width:
+				tollerance_width = box_width
+			if box_width > tollerance_height:
+				tollerance_height = box_height
 
 		cur.execute("""
 			SELECT
@@ -100,7 +112,12 @@ def line_segments_and_stops_in_boundingbox(conn, minlat, minlong, maxlat, maxlon
 
 			WHERE atcocode IN %s
 			AND point(latitude, longitude) <@ box(point(%s, %s), point(%s, %s))
-		""", (tuple(atcocode_list) or ('nothing',), minlat, minlong, maxlat, maxlong,))
+		""", (
+			tuple(atcocode_list) or ('nothing',),
+			minlat - tollerance_height,
+			minlong - tollerance_width,
+			maxlat + tollerance_height,
+			maxlong + tollerance_width,))
 		bus_stops = {
 			stop_id: {
 				"lat": latitude,
