3062226e1177e3255e69d1d2a6685196b15e6e78
David Batley <git@dbatley.com>
2018-02-28 13:37:16 +0000

    Cleanup more old-style code

--- server.py
+++ server.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 # encoding: utf8
 
-from tlparser.query_boundingbox import line_segments_and_stops_in_boundingbox
-from tlparser.query_boundingbox import line_segments_in_boundingbox
-from tlparser.query_boundingbox import journeypattern_ids_in_boundingbox
-from tlparser.query_boundingbox import line_segments_and_stops_for_journeypattern
 import psycopg2
 import json
 import logging
@@ -35,17 +31,6 @@ def statement_timeout(conn, seconds):
 	millis = int(seconds*1000)
 	with conn.cursor() as cur:
 		cur.execute("SET statement_timeout TO %s;", (millis,))
-
-def boundingbox_from_request():
-	height = float(request.args['height'])
-	width = float(request.args['width'])
-	if width * height > AREA_TOO_LARGE:
-		raise ValueError("Area too large (forbidden)")
-	return dict(
-		minlat=float(request.args['lat']),
-		minlng=float(request.args['lng']),
-		maxlat=float(request.args['lat']) - height,
-		maxlng=float(request.args['lng']) + width)
 
 @app.route('/')
 def map_page():
@@ -89,58 +74,6 @@ def postcode_complete():
 				""", (prefix,))
 			data = [postcode for [postcode] in cur.fetchall()]
 			return jsonify({"results": data})
-
-@app.route('/json/')
-def format_json():
-	boundingbox = boundingbox_from_request()
-	logging.info('%r', boundingbox)
-	with database() as conn:
-		statement_timeout(conn, 10)
-		pairs_and_stops = line_segments_and_stops_in_boundingbox(conn, min_freq=int(request.args.get('min_freq', 1)), **boundingbox)
-		return json.dumps(pairs_and_stops, indent=4)
-
-@app.route('/bbox/')
-def format_journeypattern_ids_in_boundingbox():
-	boundingbox = boundingbox_from_request()
-	logging.info('%r', boundingbox)
-	with database() as conn:
-		statement_timeout(conn, 10)
-		jpid_list = journeypattern_ids_in_boundingbox(conn, **boundingbox)
-		return jsonify({"journeypatterns": jpid_list})
-
-@app.route('/jp/<int:journeypattern_id>/')
-def format_journeypattern(journeypattern_id):
-	with database() as conn:
-		statement_timeout(conn, 10)
-		pairs_and_stops = line_segments_and_stops_for_journeypattern(conn, journeypattern_id)
-		return jsonify(pairs_and_stops)
-
-@app.route('/dot/')
-def format_dot():
-	boundingbox = boundingbox_from_request()
-	with database() as conn:
-		statement_timeout(conn, 10)
-		points = line_segments_in_boundingbox(conn, **boundingbox)
-		return 'digraph {\n' + '\n'.join(
-			'"{}" -> "{}";'.format(point['from']['id'], point['to']['id'])
-			for point in points
-		) + '\n}'
-
-@app.route('/postcode_to_ll/<postcode>')
-def postcode_to_ll(postcode):
-	with database() as conn:
-		with conn.cursor() as cur:
-			cur.execute("""
-				SELECT location[0], location[1]
-				FROM oscodepointdata
-				WHERE postcode = %s;
-				""", (postcode.strip(" ").upper(),))
-			rows = list(cur)
-			if rows:
-				[[lat, lng]] = rows
-				return jsonify({"result": True, "lat": lat, "lng": lng})
-			else:
-				return jsonify({"result": False})
 
 def _one_feature(from_id, to_id, from_lat, from_lng, to_lat, to_lng, length, frequency_array):
 	frequency_array = [float(freq) for freq in frequency_array]  # Damn you Decimal module
