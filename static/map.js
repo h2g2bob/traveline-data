8ea0c2af1d70f5d77b0e3cddf01684992eecf6ff
David Batley <git@dbatley.com>
2018-07-01 15:00:53 +0100

    Adjust attribution notice
    - link to the project description
    - more concise (abbreviate OGLv3, for example) so it's actually readable

--- static/map.js
+++ static/map.js
@@ -4,8 +4,16 @@ window.addEventListener("load", function () {
 	/* map layers */
 
 	var geo_layers = L.layerGroup().addTo(mymap);
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-	    attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery &copy; <a href=\"http://mapbox.com\">Mapbox</a>, Contains public sector information licensed under the Open Government Licence v3.0 from <a href=\"http://www.travelinedata.org.uk/\">Traveline National Dataset (TNDS)</a>, <a href=\"https://data.gov.uk/dataset/ff93ffc1-6656-47d8-9155-85ea0b8f2251/national-public-transport-access-nodes-naptan\">Naptan</a> and <a href=\"https://data.gov.uk/dataset/7dc36b99-9b5e-4475-91ab-ab16e1cabb6d/nhs-postcode-directory-latest-centroids\">NHS Postcode Directory</a>",
+	var tile_layer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	    attribution: (""
+		+ "<a href=\"https://github.com/h2g2bob/traveline-data\">h2g2bob's bus map</a>"
+		+ ". Map: <a href=\"http://openstreetmap.org\">OpenStreetMap</a> (<a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">cc-by-sa</a>)"
+		+ " / <a href=\"http://mapbox.com\">Mapbox</a>"
+		+ ". Using public sector information licensed under <a href=\"http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/\">OGLv3</a>"
+		+ " from <a href=\"http://www.travelinedata.org.uk/\">Traveline National Dataset (TNDS)</a>"
+		+ ", <a href=\"https://data.gov.uk/dataset/ff93ffc1-6656-47d8-9155-85ea0b8f2251/national-public-transport-access-nodes-naptan\">Naptan</a>"
+		+ " and <a href=\"https://data.gov.uk/dataset/7dc36b99-9b5e-4475-91ab-ab16e1cabb6d/nhs-postcode-directory-latest-centroids\">NHS Postcode Directory</a>"
+		+ "."),
 	    maxZoom: 18,
 	    id: 'mapbox.light',
 	    accessToken: 'pk.eyJ1IjoiaDJnMmJvYiIsImEiOiJjamUydDB1b3oxb3loMnFxbGdnbWZucmxlIn0.amXanuYenMfuUQxJb4ITKQ'
